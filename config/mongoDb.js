7302821cec6c30acc4129217d317e2ee63f70f26
ganesh kumar <ganeshku73@gmail.com>
2024-11-28 18:43:23 +0530

    confgi file commit

---
+++ config/mongoDb.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const mongoString = process.env.MONGODB_URI;
+
+mongoose.connect(mongoString);
+
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+
+
+database.once('connected', () => {
+    console.log('Mongo Database Connected');
+})
+
+
+module.exports = database;
